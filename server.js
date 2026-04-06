90878821bd901f1199feb2b62bc9c1edee9d1ddd
keerthana-786 <keerthanar0396@gmail.com>
2026-04-06 17:27:27 +0530

    Fix syntax error in CORS configuration

--- server.js
+++ server.js
@@ -44,7 +44,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
-}));
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
